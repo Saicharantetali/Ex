1aa68d1918d4ae0bca984cd8a9c6bfea1a16bff5
Tetali sai charan <121687670+Saicharantetali@users.noreply.github.com>
2023-10-04 08:43:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Nav from './NavBar/Nav';
+import HomePage from './Homepage/Homepage';
+import History from './History/History';
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/history' element={<History />}></Route>
+        </Routes>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
